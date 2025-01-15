cdf5ebd9534bdfbf3e25899fa583352d56a65635
hamouda2099 <telking90@gmail.com>
2025-01-15 12:40:11 +0200

    update packages

--- lib/logic/functions/device_info.dart
+++ lib/logic/functions/device_info.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/services.dart';
 
 import '../../config/constants.dart';
